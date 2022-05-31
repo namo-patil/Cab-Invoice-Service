130a1ae25121e798c2fe6a661993f09a9283c52a
namo-patil <namratapatil74738@gmail.com>
2022-06-01 01:17:38 +0530

    Added: Test case and code to return total number of rides, total fare and average fare per ride

--- src/test/java/com/bridgelabz/invoiceservice/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/invoiceservice/InvoiceGeneratorTest.java
@@ -1,12 +1,19 @@
 package com.bridgelabz.invoiceservice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -15,19 +22,19 @@ public class InvoiceGeneratorTest {
 
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
+
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        Ride[] rides = { new Ride(2.0,5),
-                         new Ride(0.1,1)
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0, 5),
+                        new Ride(0.1, 1)
                        };
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 }
