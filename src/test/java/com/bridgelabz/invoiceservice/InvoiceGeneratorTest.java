a7b6d6a9ae6a00a131f807257e0eed8fbad5cb6b
namo-patil <namratapatil74738@gmail.com>
2022-05-31 23:46:07 +0530

    Added: Test case to return minimum fare

--- src/test/java/com/bridgelabz/invoiceservice/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/invoiceservice/InvoiceGeneratorTest.java
@@ -5,11 +5,19 @@ import org.junit.Test;
 
 public class InvoiceGeneratorTest {
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare(){
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
